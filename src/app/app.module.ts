9944e7e476f16af65f0f859cf899575fdf30f9f1
Zuenda <zuenda25@gmail.com>
2022-04-24 00:53:39 +0700

    generate pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { ListComponent } from './pages/list/list.component';
+import { RoomComponent } from './pages/room/room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListComponent,
+    RoomComponent
   ],
   imports: [
     BrowserModule,
